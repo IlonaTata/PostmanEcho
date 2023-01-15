70b3b2bedfc68781271aed1e5a5b55cb7d1062d4
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2023-01-15 22:41:21 +0300

    First commit

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some name"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
